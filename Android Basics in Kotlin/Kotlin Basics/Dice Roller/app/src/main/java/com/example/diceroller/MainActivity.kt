cc97eefd9f4ace2cde5e0de0bcc6e042146729b1
Shivam-officials <suryabhardwajofficial@gmail.com>
2021-09-21 06:59:38 +0530

    added the toast for  dice roll msg and  text to show when butten get click

--- Android Basics in Kotlin/Kotlin Basics/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Android Basics in Kotlin/Kotlin Basics/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button2)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView= findViewById(R.id.textView)
+            resultTextView.text="6"
+        }
     }
 }
